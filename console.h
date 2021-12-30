f0c58e2005c9e5d974d0372e97abda90988e28d3
steven <stevenliou2016@gmail.com>
2021-12-31 03:33:12 +0800

    add console.h

---
+++ console.h
@@ -0,0 +1,17 @@
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
+
+typedef bool (*cmd_func)(int, char **);
+
+typedef struct cmd_ele{
+    char *cmd;
+    char *doc;
+    cmd_func op;
+    struct cmd_ele *next;
+}cmd_ele, *cmd_ptr;
+
+void console_init();
+bool add_cmd(char *, char *, cmd_func);
+bool run_console();
+
+#endif
